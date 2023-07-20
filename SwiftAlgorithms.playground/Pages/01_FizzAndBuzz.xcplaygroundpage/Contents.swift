6d8cbfc7d72fa0329b61a9ecaa38171753968a92
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-20 14:14:09 -0600

    Fizz and Buzz instructions

---
+++ SwiftAlgorithms.playground/Pages/01_FizzAndBuzz.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+/*:
+# 01 - Fizz And Buzz
+## Instructions:
+Write a program that prints the numbers from 1 to 100, for multiples of '3' print "Fizz" instead of the number and for
+the multiples of "5" print "Buzz". If the number is multiple of '3' and '5' print "FizzBuzz" instead of the number.
+* [Next](@next)
+*/
