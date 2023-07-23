3fc65bc44017255c860261ef8f0cbdc91107ef4a
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-22 18:59:16 -0600

    Factorial instructions

--- SwiftAlgorithms.playground/Pages/02_BinarySearch.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms.playground/Pages/02_BinarySearch.xcplaygroundpage/Contents.swift
@@ -1,10 +1,11 @@
-/*: [<- Fizz And Buzz](@previous)
+/*:
+[<- Fizz And Buzz](@previous)
 
- # 02 - Binary Search
- ## Instructions:
- Write a function that recibe an array of numbers and also recibe a number to search for. Print "The number was found" if the number is in the array or print "The number was NOT founded" if the number is not in the array.
+# 02 - Binary Search
+## Instructions:
+Write a function that recibe an array of numbers and also recibe a number to search for. Print "The number was found" if the number is in the array or print "The number was NOT founded" if the number is not in the array.
  
-[Next](@next)
+[-> Factorial](@next)
 */
 binarySearch(searchNumber: 1, searchIn: [1])
 binarySearch(searchNumber: 1, searchIn: [])
