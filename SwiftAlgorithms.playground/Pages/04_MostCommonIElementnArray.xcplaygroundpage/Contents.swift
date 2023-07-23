c640f6f0aedae56e830f01812420d5a9881e62bc
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-22 21:58:21 -0600

    Most Common Element In Array Instructions

---
+++ SwiftAlgorithms.playground/Pages/04_MostCommonIElementnArray.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+[<- Factorial](@previous)
+# 04 - Most Common Element In Array
+## Instructions:
+Write a function that recibe an array of strings and return the value that is repeted more times.
+
+[Next](@next)
+*/
