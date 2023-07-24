6d63966ec339f9daa8026af7097969afc4ae7ad7
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-24 16:34:53 -0600

    Fibonacci Recursive Algorithm

--- SwiftAlgorithms.playground/Pages/06_FibonacciSequence.xcplaygroundpage/Sources/FibonacciSequence.swift
+++ SwiftAlgorithms.playground/Pages/06_FibonacciSequence.xcplaygroundpage/Sources/FibonacciSequence.swift
@@ -2,9 +2,7 @@ import Foundation
 
 public func fibonacciSequenceFor(numberSteps: Int) -> [Int] {
     var sequence = [0,1]
-    if numberSteps <= 1 {
-        return sequence
-    }
+    if numberSteps <= 1 { return sequence }
     for _ in 0...numberSteps - 2 {
         let first = sequence[sequence.count - 2]
         guard let second = sequence.last else { return [] }
@@ -13,3 +11,18 @@ public func fibonacciSequenceFor(numberSteps: Int) -> [Int] {
     return sequence.dropLast()
 }
 
+public func fibonacciRecursionSequenceFor(numberSteps: Int, first: Int = 0, second: Int = 1) -> [Int] {
+    if numberSteps == 0 { return [] }
+    return [first + second] + fibonacciRecursionSequenceFor(numberSteps: numberSteps - 1,
+                                                            first: second,
+                                                            second: first + second)
+}
+
+public func fibonacciRecursion(sizeSequence: Int) -> [Int] {
+    if sizeSequence <= 2 { return [0,1] }
+    var fibonacciSequence = [0,1] + fibonacciRecursionSequenceFor(numberSteps: sizeSequence)
+    for _ in 0..<2 {
+        fibonacciSequence = fibonacciSequence.dropLast()
+    }
+    return fibonacciSequence
+}
