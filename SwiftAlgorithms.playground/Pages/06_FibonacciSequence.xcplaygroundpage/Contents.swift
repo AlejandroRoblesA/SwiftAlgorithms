d5906f275f355d4125bf4c5cf791f8d0df112a37
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-24 13:42:33 -0600

    Fibonacci Instructions

---
+++ SwiftAlgorithms.playground/Pages/06_FibonacciSequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+/*:
+[<- Reverse Words](@previous)
+# 06 - Fibonacci Sequence
+## Instructions:
+Write a function that recieve a number and return an array with the size of the number recieved by parameter with the fibonacci sequence.
+
+![Fibonacci](Fibonacci.png)
+
+ [Next](@next)
+*/
