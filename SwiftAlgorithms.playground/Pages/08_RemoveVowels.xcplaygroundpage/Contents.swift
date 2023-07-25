c4eee5d58b05422b2adb0831f4a9d953ee0ba4e5
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-25 12:52:00 -0600

    Remove Vowels Instructions

---
+++ SwiftAlgorithms.playground/Pages/08_RemoveVowels.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+[<- Sum Of Digits](@previous)
+# 08 - Remove Vowels
+## Instrcutions:
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+ 
+Example:
+The string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!"
+ 
+[Next](@next)
+*/
