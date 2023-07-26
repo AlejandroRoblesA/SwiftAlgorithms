52f1b39a8ff7615e62b2d9c55b5b5e1772e30e38
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-26 16:53:02 -0600

    Reduce Directions Algorithm Using Reduce clousure

--- SwiftAlgorithms.playground/Pages/10_ReduceDirections.xcplaygroundpage/Sources/ReduceDirections.swift
+++ SwiftAlgorithms.playground/Pages/10_ReduceDirections.xcplaygroundpage/Sources/ReduceDirections.swift
@@ -1,5 +1,11 @@
 import Foundation
 
+public let nssewnw = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+public let nsew = ["NORTH", "SOUTH", "EAST", "WEST"]
+public let newsww = ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"]
+public let eewnweesnw = ["EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"]
+public let nwse = ["NORTH", "WEST", "SOUTH", "EAST"]
+
 public func reduceDirections(from array: [String]) -> [String] {
     var directions: [String] = array
     for index in 0..<directions.count where index < directions.count-1 {
@@ -31,5 +37,15 @@ private func canReduce(currentDirection: String, lastDirection: String) -> Bool 
         case ("WEST", "EAST") : return true
         case ("EAST", "WEST") : return true
         default: return false
+    }
+}
+
+public func reduceDirectionsArray(_ array: [String]) -> [String] {
+    return array.reduce(into: [String]()) { (result, element) in
+        if let last = result.last, [("NORTH", "SOUTH"), ("EAST", "WEST")].contains(where: { $0 == (last, element) || $0 == (element, last) }) {
+            result.removeLast()
+        } else {
+            result.append(element)
+        }
     }
 }
