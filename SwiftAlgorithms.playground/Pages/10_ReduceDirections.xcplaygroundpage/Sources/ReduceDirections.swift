c3ec42e5e25129b59c60b404156e5078bd580a2e
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-26 15:17:17 -0600

    Reduce Directions Algorithm Using Swift Validations

--- SwiftAlgorithms.playground/Pages/10_ReduceDirections.xcplaygroundpage/Sources/ReduceDirections.swift
+++ SwiftAlgorithms.playground/Pages/10_ReduceDirections.xcplaygroundpage/Sources/ReduceDirections.swift
@@ -11,3 +11,25 @@ public func reduceDirections(from array: [String]) -> [String] {
     }
     return directions
 }
+
+public func reduceDirectionsFrom(_ array: [String]) -> [String] {
+    var newDirections = [String]()
+    for direction in array {
+        if let lastDirection = newDirections.last, !newDirections.isEmpty && canReduce(currentDirection: direction, lastDirection: lastDirection) {
+            newDirections.removeLast()
+        } else {
+            newDirections.append(direction)
+        }
+    }
+    return newDirections
+}
+
+private func canReduce(currentDirection: String, lastDirection: String) -> Bool {
+    switch (currentDirection, lastDirection) {
+        case ("NORTH", "SOUTH") : return true
+        case ("SOUTH", "NORTH") : return true
+        case ("WEST", "EAST") : return true
+        case ("EAST", "WEST") : return true
+        default: return false
+    }
+}
