99370ee14b8f9b34945d4e263fdc6e80db0bfb85
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-03 17:49:38 -0600

    Reduce Instructions

---
+++ SwiftAlgorithms.playground/Pages/16_Reduce.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+[<- Map](@previous)
+# 16 - Reduce
+## Instrcutions:
+ Write a function that receive an array of Integers and return a Integer with the values of the array received additioned.
+ 
+[-> Next](@next)
+*/
