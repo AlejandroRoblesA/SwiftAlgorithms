f6f5d55e38d19b4d1568d79c09baeff7a487519b
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-03 17:46:05 -0600

    Map Instructions

---
+++ SwiftAlgorithms.playground/Pages/16_Map.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+[<- Filter](@previous)
+# 16 - Map
+## Instrcutions:
+ Write a function that receive an array of Integers, a number to multiply the array received and return a new array with the values multiplied.
+ 
+[-> Reduce](@next)
+*/
