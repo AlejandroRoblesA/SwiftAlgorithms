d7bafb991d6ba2647a2e58616fed050905d2c4a0
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-06 22:32:56 -0600

    Matriz Multiplication Algorithm

--- SwiftAlgorithms.playground/Pages/18_MatrixMultiplication.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms.playground/Pages/18_MatrixMultiplication.xcplaygroundpage/Contents.swift
@@ -1,14 +1,22 @@
-
+import UIKit
 
 func matrixMultiplication(_ a:[[Int]], _ b:[[Int]]) -> [[Int]] {
     var result = a
-    result[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0]
-    result[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1]
-    result[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0]
-    result[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1]
+    for i in 0..<a.count {
+        for j in 0..<a.count {
+            result[i][j] = 0
+        }
+    }
+    for i in 0..<a.count {
+        for j in 0..<a.count {
+            for k in 0..<a.count {
+                result[i][j] = result[i][j] + a[i][k] * b[k][j]
+            }
+        }
+    }
     return result
 }
 
-let a = [[1,2],[3,2]]
-let b = [[3,2],[1,1]]
+let a = [[2,0,1],[3,0,0],[5,1,1]]
+let b = [[1,0,1],[1,2,1],[1,1,0]]
 print(matrixMultiplication(a,b))
